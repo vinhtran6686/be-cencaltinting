70a55f225fd1186de424e87d8078ac5b58c96351
vinhtran6686 <vinhtran.6686@gmail.com>
2025-04-02 07:18:10 +0700

    Update healthcheck

--- src/heatlth/health.controller.ts
+++ src/heatlth/health.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
 import { Public } from '@/shared/decorators/auth.decorator';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
+    private http: HttpHealthIndicator,
   ) {}
 
   @Get()
   @Public()
   @HealthCheck()
   check() {
-    return this.health.check([]);
+    return this.health.check([
+      // Đây là một health check đơn giản chỉ kiểm tra xem ứng dụng có hoạt động không
+      () => Promise.resolve({ api: { status: 'up' } }),
+    ]);
   }
 }
--- src/heatlth/health.module.ts
+++ src/heatlth/health.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 import { SharedModule } from '@/shared/modules/shared.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TerminusModule, SharedModule],
+  imports: [TerminusModule, SharedModule, HttpModule],
   controllers: [HealthController],
   providers: [],
 })
